19c0b6aca131e1396ee72fc7eca3b8df5ec142c5
filipemartinsdev <filipemartins.pessoal@gmail.com>
2025-03-21 17:42:59 -0300

    fix code
    change type check logic in the function 'checkNumber'

--- lista_1/1.py
+++ lista_1/1.py
@@ -1,7 +1,7 @@
 # Escreva um programa que receba um número e verifique se ele é positivo, negativo ou zero.
 
 def checkNumber(n):
-    if(type(n) != int and type(n) != int):
+    if(type(n) != int and type(n) != float):
         return TypeError
     if(n < 0):
         return "n < 0"
@@ -17,6 +17,3 @@ print(checkNumber("10"))
 # Mudanças recomendadas:
 # - Função isinstance(value, type) para checagem de tipo;
 # - Uso de else ao fim do algoritmo;
-
-
-
